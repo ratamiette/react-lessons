c12448175986a44db78dd66b29e17f6a72b51216
Raphaela Tamiette <ratamiette@gmail.com>
2020-10-21 23:18:58 -0300

    Converted NavBar class component to Stateless Functional Component.

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+/**
+ * Converted NavBar class component to Stateless Functional Component.
+ * The reference to "this" only works in class components, in functional components, we need to add props as a parameter.
+ * React will pass the props object as an argument to this object at run time.
+ */
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
