3f4d7b9fd24a84b35bc583a54cbbc3e58a88e9da
Raphaela Tamiette <ratamiette@gmail.com>
2020-10-27 14:20:03 -0300

    Abstracting low level elements: Created MoviesTable component.

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
-
+import MoviesTable from "./moviesTable";
 import { getGenres } from "../services/fakeGenreService";
 import { getMovies } from "../services/fakeMovieService";
-
-import Like from "./common/like";
 import Pagination from "./common/pagination";
 import ListGroup from "./common/listGroup";
-
 import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
@@ -51,47 +48,11 @@ class Movies extends Component {
         </div>
         <div className="col">
           <h2>Showing {filtered.length} movies in the Database.</h2>
-
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Genre</th>
-                <th scope="col">Stock</th>
-                <th scope="col">Rate</th>
-                <th scope="col"></th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map((movie) => {
-                return (
-                  <tr key={movie._id}>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td>
-                      <Like
-                        liked={movie.liked}
-                        onToggleLike={() => this.handleLike(movie)}
-                      />
-                    </td>
-                    <td>
-                      <button
-                        type="button"
-                        onClick={() => this.handleDelete(movie)}
-                        className="btn btn-danger btn-sm"
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-
+          <MoviesTable
+            movies={movies}
+            onLike={this.handleLike}
+            onDelete={this.handleDelete}
+          />
           <Pagination
             itemsCount={filtered.length}
             pageSize={pageSize}
---
+++ vidly/src/components/moviesTable.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Like from "./common/like";
+
+const MoviesTable = (props) => {
+  const { movies, onLike, onDelete, onSort } = props;
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Genre</th>
+          <th scope="col">Stock</th>
+          <th scope="col">Rate</th>
+          <th scope="col"></th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => {
+          return (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <Like liked={movie.liked} onToggleLike={() => onLike(movie)} />
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() => onDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default MoviesTable;
