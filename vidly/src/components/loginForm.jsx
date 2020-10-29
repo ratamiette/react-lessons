5d4cc9e74d05f3c6be5711a77b5096d885e7dfd0
Raphaela Tamiette <ratamiette@gmail.com>
2020-10-29 16:11:40 -0300

    Basic form validation.

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -10,9 +10,10 @@ class LoginForm extends Component {
       username: "",
       password: "",
     },
+    errors: {},
   };
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -21,12 +22,14 @@ class LoginForm extends Component {
             name="username"
             value={account.username}
             label="Username"
+            error={errors.username}
             onChange={this.handleChange}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
+            error={errors.password}
             onChange={this.handleChange}
           />
           <button className="btn btn-primary">Login</button>
@@ -38,13 +41,32 @@ class LoginForm extends Component {
   handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
+  };
+
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = (e) => {
     // it will prevent full page reload
     e.preventDefault();
 
+    const errors = this.validate();
+
+    this.setState({ errors: errors || {} });
+
+    if (errors) return null;
+
+    // Call the server to persist data
     console.log("Submited");
   };
 }
