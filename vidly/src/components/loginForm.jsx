06047a30b9943bb1059bbc4ba6b77d640049e820
Raphaela Tamiette <ratamiette@gmail.com>
2020-10-29 15:40:18 -0300

    Handling multiple Inputs. Extracting a reusable input.

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,38 +1,49 @@
 import React, { Component } from "react";
+import Input from "./common/input";
 
 class LoginForm extends Component {
-  //   username = React.createRef(); // create a reference to use in the input field
-
+  // username = React.createRef(); // create a reference to use in the input field. e.g:
+  // <input ref={this.username} />
+  // get a reference of the element in the DOM: const username = this.username.current.value;
+  state = {
+    account: {
+      username: "",
+      password: "",
+    },
+  };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              //   ref={this.username}
-              id="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
     );
   }
 
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   handleSubmit = (e) => {
     // it will prevent full page reload
     e.preventDefault();
-
-    // get a reference of the element in the DOM
-    // const username = this.username.current.value;
 
     console.log("Submited");
   };
