d2d868b1f58f0c6b3f47407d8ed9b4318d7d0103
Raphaela Tamiette <ratamiette@gmail.com>
2020-10-29 17:25:04 -0300

    Added joi browser package to validate forms.

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -12,6 +13,12 @@ class LoginForm extends Component {
     },
     errors: {},
   };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
   render() {
     const { account, errors } = this.state;
     return (
@@ -32,7 +39,9 @@ class LoginForm extends Component {
             error={errors.password}
             onChange={this.handleChange}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
@@ -41,19 +50,27 @@ class LoginForm extends Component {
   handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account, errors });
+    this.setState({ account });
   };
 
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
+
     const errors = {};
-    const { account } = this.state;
+    error.details.map((error) => {
+      errors[error.path[0]] = error.message;
+    });
+    return errors;
+  };
 
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleSubmit = (e) => {
