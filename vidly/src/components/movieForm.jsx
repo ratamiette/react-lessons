364eca257fe7df61c474d31f23a2ce3bf48d36c3
Raphaela Tamiette <ratamiette@gmail.com>
2020-11-02 12:54:27 -0300

    Refactoring MovieForm component.

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -32,20 +32,26 @@ class MovieForm extends Form {
       .label("Daily Rental Rate"),
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovie() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
+
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (exception) {
       if (exception.response && exception.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
