e359e0d35e82550a1da5275d971a23c2231fe2d1
Raphaela Tamiette <ratamiette@gmail.com>
2020-11-02 13:36:47 -0300

    Handled saveMovie.

--- vidly/src/services/movieService.js
+++ vidly/src/services/movieService.js
@@ -3,18 +3,27 @@ import { apiBaseUrl } from "../config.json";
 
 const endpoint = `${apiBaseUrl}/movies`;
 
+function movieUrl(id) {
+  return `${endpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(endpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(`${endpoint}/${movieId}`);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
-  // @TODO
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
+  }
+  return http.post(endpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(`${endpoint}/${movieId}`);
+  return http.delete(movieUrl(movieId));
 }
