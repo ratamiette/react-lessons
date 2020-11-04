3b38e56e33eeff4cdd376c90fd3c07f0b2d58fec
Raphaela Tamiette <ratamiette@gmail.com>
2020-11-04 15:01:42 -0300

    Consuming the users api into useEffect hook.

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Movie from "./hoc/Movie";
-import Counter from './hooks/Counter';
+import Counter from "./hooks/Counter";
+import Users from "./hooks/Users";
 
 function App() {
   // return <Movie id="1" />;
-  return <Counter />
+  // return <Counter />
+  return <Users />;
 }
 
 export default App;
---
+++ react-advanced/src/hooks/Users.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Users(props) {
+  const [users, setUsers] = useState([]);
+
+  // the callback function that we pass as first argument to useEffect() can't be decorated with async. We need to encapsulate into a new function
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(data);
+    }
+    getUsers();
+  });
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
